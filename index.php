2ad19c22435caf062d21aac6fa5a5aa3e7568c71
福島碧 <>
2021-08-11 18:02:05 +0900

    no message

--- index.php
+++ index.php
@@ -6,10 +6,16 @@
     <body>
         <?php 
             for ($i = 5; $i < 101; $i++) {
-                $body==""
-                if($i%7==0){$body = "Fizz"}
-                if($i%5==0){$body += "Buzz";}
-                if($body==""){$body =$i;}
+                $body="";
+                if($i%7==0){
+                    $body = "Fizz";
+                }
+                if($i%5==0){
+                    $body += "Buzz";
+                }
+                if($body==""){
+                    $body =$i;
+                }
                 echo $body;
             }
         ?>
