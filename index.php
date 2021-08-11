c8d0f1123cd9657249d7e033e8dd1deb4e35d4c1
福島碧 <>
2021-08-11 18:00:22 +0900

    no message

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     </head>
     <body>
         <?php 
-            echo "Hello World!";
             for ($i = 5; $i < 101; $i++) {
+                $body==""
                 if($i%7==0){$body = "Fizz"}
                 if($i%5==0){$body += "Buzz";}
                 if($body==""){$body =$i;}
